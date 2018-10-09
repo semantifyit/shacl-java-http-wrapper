b76f208bae8579e27e1c116ea54a4add0282548b
Umutcan Simsek <s.umutcan@gmail.com>
2018-10-09 08:03:25 +0000

    initial commit with source

---
+++ README.md
@@ -0,0 +1,5 @@
+# SHACL Java HTTP Wrapper
+
+This is a small application that exposes basic SHACL validation functionality from Topbraid SHACL Java API over HTTP.
+
+See https://github.com/TopQuadrant/shacl for more information regarding SHACL and the API itself.
---
+++ shacl-validator/src/main/java/org/sti2/lib/Validator.java
@@ -0,0 +1,25 @@
+package org.sti2.lib;
+
+import java.net.URI;
+
+import org.apache.jena.rdf.model.Model;
+
+/**
+ * Validator
+ */
+public class Validator {
+
+    Model dataModel;
+    Model shapeModel;
+
+
+    public Validator(String dataPath, String shapePath)
+    {
+        
+    }
+
+    public Validator(URI dataUri, URI shapeUri)
+    {
+        
+    }
+}
